c84d0442255714d117141adaa980e7e1eee6b4a8
Saji Weerasingham <saji.weerasingham@outlook.com>
2025-02-27 04:30:42 +1000

    wip

--- Aspire.Pizzeria.Data/Migrations/20250220181348_PizzaOrder.cs
+++ Aspire.Pizzeria.Data/Migrations/20250220181348_PizzaOrder.cs
@@ -1,98 +1,96 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Aspire.Pizzeria.Data.Migrations
+namespace Aspire.Pizzeria.Data.Migrations;
+
+/// <inheritdoc />
+public partial class PizzaOrder : Migration
 {
     /// <inheritdoc />
-    public partial class PizzaOrder : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Orders",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    CustomerName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
-                    DeliveryAddress = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
-                    IsPrepared = table.Column<bool>(type: "boolean", nullable: false),
-                    PreparationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
-                    IsDelivered = table.Column<bool>(type: "boolean", nullable: false),
-                    DeliveryDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Orders", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Orders",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                CustomerName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                DeliveryAddress = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                IsPrepared = table.Column<bool>(type: "boolean", nullable: false),
+                PreparationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                IsDelivered = table.Column<bool>(type: "boolean", nullable: false),
+                DeliveryDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Orders", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Pizzas",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
-                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
-                    Price = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Pizzas", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Pizzas",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "integer", nullable: false)
+                    .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                Price = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Pizzas", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "OrderPizza",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
-                    PizzaId = table.Column<int>(type: "integer", nullable: false),
-                    Quantity = table.Column<int>(type: "integer", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_OrderPizza", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_OrderPizza_Orders_OrderId",
-                        column: x => x.OrderId,
-                        principalTable: "Orders",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_OrderPizza_Pizzas_PizzaId",
-                        column: x => x.PizzaId,
-                        principalTable: "Pizzas",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "OrderPizza",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                PizzaId = table.Column<int>(type: "integer", nullable: false),
+                Quantity = table.Column<int>(type: "integer", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OrderPizza", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_OrderPizza_Orders_OrderId",
+                    column: x => x.OrderId,
+                    principalTable: "Orders",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_OrderPizza_Pizzas_PizzaId",
+                    column: x => x.PizzaId,
+                    principalTable: "Pizzas",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_OrderPizza_OrderId",
-                table: "OrderPizza",
-                column: "OrderId");
+        migrationBuilder.CreateIndex(
+            name: "IX_OrderPizza_OrderId",
+            table: "OrderPizza",
+            column: "OrderId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_OrderPizza_PizzaId",
-                table: "OrderPizza",
-                column: "PizzaId");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_OrderPizza_PizzaId",
+            table: "OrderPizza",
+            column: "PizzaId");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "OrderPizza");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "OrderPizza");
 
-            migrationBuilder.DropTable(
-                name: "Orders");
+        migrationBuilder.DropTable(
+            name: "Orders");
 
-            migrationBuilder.DropTable(
-                name: "Pizzas");
-        }
+        migrationBuilder.DropTable(
+            name: "Pizzas");
     }
 }
