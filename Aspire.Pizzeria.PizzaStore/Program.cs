a3948c36eef60bc7beaa4b41bbffcd9abec44aa5
Saji Weerasingham <saji.weerasingham@outlook.com>
2025-02-28 05:42:27 +1000

    wip

--- Aspire.Pizzeria.PizzaStore/Program.cs
+++ Aspire.Pizzeria.PizzaStore/Program.cs
@@ -1,8 +1,6 @@
-using System.Net.Sockets;
 using Aspire.Pizzeria.Data;
+using Aspire.Pizzeria.PizzaStore;
 using FastEndpoints;
-using Microsoft.EntityFrameworkCore;
-using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,41 +26,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-using (var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(3)))
-using (var serviceScope = app.Services.CreateScope())
-{
-    var dbContext = serviceScope.ServiceProvider.GetRequiredService<PizzeriaDbContext>();
-    var connectionString = dbContext.Database.GetConnectionString();
-    var dbBuilder = new NpgsqlConnectionStringBuilder(connectionString);
-
-    if (string.IsNullOrWhiteSpace(dbBuilder?.Host))
-    {
-        throw new InvalidOperationException("Failed to parse connection string.");
-    }
-
-    while (!cancellationTokenSource.Token.IsCancellationRequested)
-    {
-        try
-        {
-            using (var tcpClient = new TcpClient())
-            {
-                await tcpClient.ConnectAsync(dbBuilder.Host, dbBuilder.Port);
-            }
-
-            await dbContext.Database.MigrateAsync(cancellationTokenSource.Token);
-
-            var seeder = serviceScope.ServiceProvider.GetRequiredService<PizzeriaSeeder>();
-            await seeder.SeedAsync(cancellationTokenSource.Token);
-
-            break;
-        }
-        catch (Exception ex)
-        {
-            app.Logger.LogError(ex, "Failed to migrate database. Retrying in 5 seconds...");
-
-            await Task.Delay(TimeSpan.FromSeconds(5), cancellationTokenSource.Token);
-        }
-    }
-}
+await app.MigrateDatabaseAsync();
 
 await app.RunAsync();
---
+++ Aspire.Pizzeria.PizzaStore/WebApplicationExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Aspire.Pizzeria.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Net.Sockets;
+
+namespace Aspire.Pizzeria.PizzaStore;
+
+internal static class WebApplicationExtensions
+{
+    public static async Task MigrateDatabaseAsync(this WebApplication app)
+    {
+        using (var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(3)))
+        using (var serviceScope = app.Services.CreateScope())
+        {
+            var dbContext = serviceScope.ServiceProvider.GetRequiredService<PizzeriaDbContext>();
+            var connectionString = dbContext.Database.GetConnectionString();
+            var dbBuilder = new NpgsqlConnectionStringBuilder(connectionString);
+
+            if (string.IsNullOrWhiteSpace(dbBuilder?.Host))
+            {
+                throw new InvalidOperationException("Failed to parse connection string.");
+            }
+
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                try
+                {
+                    using (var tcpClient = new TcpClient())
+                    {
+                        await tcpClient.ConnectAsync(dbBuilder.Host, dbBuilder.Port);
+                    }
+
+                    await dbContext.Database.MigrateAsync(cancellationTokenSource.Token);
+
+                    var seeder = serviceScope.ServiceProvider.GetRequiredService<PizzeriaSeeder>();
+                    await seeder.SeedAsync(cancellationTokenSource.Token);
+
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    app.Logger.LogError(ex, "Failed to migrate database. Retrying in 5 seconds...");
+
+                    await Task.Delay(TimeSpan.FromSeconds(5), cancellationTokenSource.Token);
+                }
+            }
+        }
+    }
+}
